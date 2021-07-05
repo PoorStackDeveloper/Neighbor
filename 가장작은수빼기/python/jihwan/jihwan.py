d5abc2df0c8eac39e8b843def9cac2eab1874ac9
johnjihwan <s20062@gsm.hs.kr>
2021-07-05 21:05:05 +0900

    resolve the questionm

--- 가장작은수빼기/python/jihwan/jihwan.py
+++ 가장작은수빼기/python/jihwan/jihwan.py
@@ -1,4 +1,10 @@
-def solution(){
+def solution(arr):
+    min = arr[0]
+    for i in range(len(arr)):
+        if(arr[i] < min):
+            min = arr[i].value
+    arr.remove(min)
+    print(arr)
 
-}
-arr = [1,2,3,4,5];
+arr = [1,2,3,4,5]
+solution(arr)
