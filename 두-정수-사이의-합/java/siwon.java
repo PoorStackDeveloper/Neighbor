d3c47a52bd9463e85a98989ed657aed52179aad8
siwony_ <siwon103305@gmail.com>
2021-07-06 16:08:13 +0900

    [FIX] 실수로 규칙 따윈 무시해버림

---
+++ 두-정수-사이의-합/java/siwon.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(int a, int b) {
+        if(a == b) 
+            return (long)a;
+        if(a > b)
+            return (b - a) * (a + b);
+        else
+
+    }
+        
+}
